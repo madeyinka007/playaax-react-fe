d8374df0e843d9c07f269192732ecaf62bac8979
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-15 23:32:49 +0100

    Complete the Author,Post and category delete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from "react";
 import { BoldArrowIcon, ClockIcon, HeadingsIcon } from "../assets/SvgsIcons";
 import { Button } from "../components/Form/Button";
-import Header from "../components/Header";
-import Headlines from "../components/Headlines";
+// import Header from "../components/Header";
+// import Headlines from "../components/Headlines";
 import HeroSlider from "../components/HeroSlider";
 import Ads1 from "src/assets/images/T&C.png";
 import HookForm from "src/components/Form/Form";
@@ -46,6 +46,7 @@ import LoadingSpinner from "src/components/Loading/LoadingSpinner";
 import { Link } from "react-router-dom";
 import { fetchCategorys } from "src/Redux/category/categoriesThunk";
 import CategoryItem from "src/components/ui/CategoryItem";
+import classNames from "classnames";
 
 export const categoryData = [
   { name: "Premier League", icon: Premier },
@@ -63,6 +64,11 @@ export const categoryData = [
 
 const Home = () => {
   const dispatch = useDispatch();
+
+  const [loadMore, setLoadMore] = useState(3);
+
+  const toggleMore = () => setLoadMore(loadMore + 3);
+
   const airtimeData = [
     { name: "MTN", network: "mtn", avatar: mtn },
     { name: "GLO", network: "glo", avatar: glo },
@@ -92,11 +98,11 @@ const Home = () => {
   ];
   const [selectedAirtime, setSelectedAirtime] = useState(airtimeData[0]);
 
-  const { posts, post, loading, error } = useSelector((state) => state.posts);
-  // console.log("all posts", posts);
+  const { posts, loading } = useSelector((state) => state.posts);
+  // console.log("all posts", posts?.length);
 
   const { categories } = useSelector((state) => state.category);
-  console.log("all categories", categories);
+  // console.log("all categories", categories);
 
   const fetchCategorysHandler = () => {
     dispatch(fetchCategorys());
@@ -115,7 +121,7 @@ const Home = () => {
   const randomIndex = Math.floor(Math.random() * posts.length);
   const randomPost = posts[randomIndex];
 
-  console.log("randomPost", randomPost);
+  // console.log("randomPost", randomPost);
 
   const fetchPostsHandler = () => {
     dispatch(fetchPosts());
@@ -145,11 +151,11 @@ const Home = () => {
   };
   return (
     <>
-      <Header />
+      {/* <Header />
       <div className="pt-[76px]">
         <Headlines />
-      </div>
-      <div className="px-4 bg-gray-100">
+      </div> */}
+      <div className="px-4 bg-gray-100 pb-20">
         <img
           src="/imgs/ads-banner-1.png"
           alt="ads-banner"
@@ -438,7 +444,7 @@ const Home = () => {
                         ? lastPost?.image
                         : "https://images.unsplash.com/photo-1631451095765-2c91616fc9e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                     }
-                    className="h-64 lg:h-full w-full rounded-xl object-cover shadow-xl transition "
+                    className="h-64 lg:h-[500px] w-full rounded-xl object-cover shadow-xl transition "
                   />
 
                   <div className="p-4">
@@ -475,6 +481,89 @@ const Home = () => {
                   </div>
                 </article>
               )}
+
+              <div className=" flex flex-col space-y-6">
+                {loading ? (
+                  <LoadingSpinner />
+                ) : (
+                  <>
+                    {posts
+                      ?.slice(0, loadMore)
+                      ?.map((mainPost, mainPostIndex) => (
+                        <article key={mainPostIndex} className="group">
+                          <img
+                            alt=""
+                            src={
+                              mainPost?.image
+                                ? mainPost?.image
+                                : "https://images.unsplash.com/photo-1631451095765-2c91616fc9e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                            }
+                            className="h-64 lg:h-[500px] w-full rounded-xl object-cover shadow-xl transition "
+                          />
+
+                          <div className="p-4">
+                            <Link to={`/posts/${mainPost?._id}`}>
+                              <h3 className="text-lg lg:text-xl xl:text-2xl font-semibold line-clamp-2 pb-2 text-gray-800 hover:text-primary-800 cursor-pointer">
+                                {mainPost?.title}
+                              </h3>
+                            </Link>
+                            <Link to={`/posts/${mainPost?._id}`}>
+                              <h3 className="text-base  line-clamp-2 text-gray-600 hover:text-primary-800 cursor-pointer">
+                                {mainPost?.short_content}
+                              </h3>
+                            </Link>
+                            {/* <div className=" flex items-center gap-2 text-sm pb-4 text-gray-600">
+                            <ClockIcon className="w-4 h-4" />
+
+                            <p className="py-2">
+                              {formatDateTime(mainPost?.createdAt)}
+                            </p>
+                          </div> */}
+                            <div className="pt-8">
+                              <hr />
+                            </div>
+                          </div>
+                        </article>
+                      ))}
+                  </>
+                )}
+
+                <div
+                  className={classNames(
+                    "w-full flex items-center justify-center pb-4",
+                    { hidden: loadMore >= posts?.length }
+                  )}
+                >
+                  <Button
+                    onClick={() => toggleMore()}
+                    leftIcon={
+                      <div role="status">
+                        <svg
+                          aria-hidden="true"
+                          className="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-primary-800"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    }
+                    size="lg"
+                    rounded
+                  >
+                    Show me More
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="w-full lg:w-1/4 space-y-6">
