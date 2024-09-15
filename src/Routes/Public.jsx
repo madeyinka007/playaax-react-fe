4434643ca9450ae9eb1dca077dbf26555ab0ceb9
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-15 23:33:22 +0100

    Complete the Author,Post and category delete

--- src/Routes/Public.jsx
+++ src/Routes/Public.jsx
@@ -3,9 +3,21 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import Loading from "../components/Loading/Loading";
+import Header from "../components/Header";
+import Footer from "../components/layout/Footer";
+import Headlines from "../components/Headlines";
 
 const Public = () => {
-  return <Outlet />;
+  return (
+    <div>
+      <Header />
+      <div className="pt-[76px]">
+        <Headlines />
+      </div>
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Public;
